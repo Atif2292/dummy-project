c2b2cf6892c0dc13c4c5afddc1688ed4b09fc5df
Md Atif <141242287+Atif2292@users.noreply.github.com>
2025-05-21 13:31:48 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -16,7 +16,7 @@ function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://127.0.0.1:5000/api/signup', {
+      const res = await fetch('https://dummy-2-7njc.onrender.com/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
