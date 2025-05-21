a9c8e91d61b80dd244f32fb2753da9b5417b8562
Md Atif <141242287+Atif2292@users.noreply.github.com>
2025-05-21 13:30:39 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://127.0.0.1:5000/api/login', {
+    const response = await fetch('https://dummy-2-7njc.onrender.com/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
